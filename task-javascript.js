7a6780607f7c6088d78b04114cb936290d0d6298
KOSUKE <skyworkerdaze@gmail.com>
2022-09-28 01:23:17 +0900

    1回目

---
+++ task-javascript.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  const btn = this.document.getElementById("click-change");
+  const techgeek = this.document.getElementById("techgeek-header");
+  btn.addEventListener("click", function () {
+    techgeek.innerHTML = "あいうえお";
+  });
+});
